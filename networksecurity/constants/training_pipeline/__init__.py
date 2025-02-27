3884296222f9aa91b2ae15c72395891f3fda16b4
urav <uravfarooqui3@gmail.com>
2025-02-27 22:17:05 +0530

    Implement data transformation functionality and update logging for data processing steps

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -49,7 +49,7 @@ DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
 
-## kkn imputer to replace nan values
+## kkn imputer to replace nan values with the mean of the nearest neighbors
 DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "missing_values": np.nan,
     "n_neighbors": 3,
