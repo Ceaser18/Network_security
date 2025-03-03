a23f0379e3c7a2dfe1284e945fe9a198766ae072
urav <uravfarooqui3@gmail.com>
2025-03-03 21:08:47 +0530

    Add MLflow model tracking and update requirements for model artifacts

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -6,7 +6,7 @@ from networksecurity.logging.logger import logging
 
 from networksecurity.entity.artifact_entity import DataTransformationArtifact,ModelTrainerArtifact
 from networksecurity.entity.config_entity import ModelTrainerConfig
-
+import mlflow
 
 
 from networksecurity.utils.ml_utils.model.estimator import NetworkModel
@@ -32,6 +32,18 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
+    
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision=classificationmetric.precision_score
+            recall=classificationmetric.recall_score
+
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision)
+            mlflow.log_metric("recall",recall)
+            mlflow.sklearn.log_model(best_model,"model")
 
 
     def train_model(self,X_train,y_train,x_test,y_test):
@@ -85,9 +97,14 @@ class ModelTrainer:
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
         
         ##track the mlflow
+        self.track_mlflow(best_model,classification_train_metric)
+
+
 
         y_test_pred=best_model.predict(x_test)
         classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
+
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
         
